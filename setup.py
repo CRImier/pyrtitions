2a973511f9dc443bbfc7954d5d0f6b9f098fffc4
CRImier <crimier@yandex.ru>
2016-11-19 02:51:35 +0200

    New version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'pyrtitions',
   packages = ['pyrtitions'],
-  version = 'v1.0',
-  description = 'A library to scan partitions and see where they are mounted',
+  version = 'v1.1',
+  description = 'A library for Linux to get partition information',
   author = 'Arsenijs Picugins',
   author_email = 'crimier@yandex.ru',
   url = 'https://github.com/CRImier/pyrtitions',
-  download_url = 'https://github.com/CRImier/pyrtitions/tarball/v1.0',
+  download_url = 'https://github.com/CRImier/pyrtitions/tarball/v1.1',
   keywords = ['linux', 'partitions'],
   classifiers = [],
 )
